98d671c99680260aeb9fa6fe2665b89df65ce618
minion <minion>
2022-02-16 07:08:22 +0000

    with 💕

---
+++ contracts/testnet/16/16535571d2b7fabcd3c9ecfed030c650809287a7_PayoutHelper.sol
@@ -0,0 +1,116 @@
+/**
+ *Submitted for verification at arbiscan.io on 2022-02-15
+*/
+
+// File: contracts/Lib/LibDerivative.sol
+
+pragma solidity 0.5.16;
+pragma experimental ABIEncoderV2;
+
+/// @title Opium.Lib.LibDerivative contract should be inherited by contracts that use Derivative structure and calculate derivativeHash
+contract LibDerivative {
+    // Opium derivative structure (ticker) definition
+    struct Derivative {
+        // Margin parameter for syntheticId
+        uint256 margin;
+        // Maturity of derivative
+        uint256 endTime;
+        // Additional parameters for syntheticId
+        uint256[] params;
+        // oracleId of derivative
+        address oracleId;
+        // Margin token address of derivative
+        address token;
+        // syntheticId of derivative
+        address syntheticId;
+    }
+
+    /// @notice Calculates hash of provided Derivative
+    /// @param _derivative Derivative Instance of derivative to hash
+    /// @return derivativeHash bytes32 Derivative hash
+    function getDerivativeHash(Derivative memory _derivative) public pure returns (bytes32 derivativeHash) {
+        derivativeHash = keccak256(abi.encodePacked(
+            _derivative.margin,
+            _derivative.endTime,
+            _derivative.params,
+            _derivative.oracleId,
+            _derivative.token,
+            _derivative.syntheticId
+        ));
+    }
+}
+
+// File: contracts/Interface/IDerivativeLogic.sol
+
+pragma solidity 0.5.16;
+
+/// @title Opium.Interface.IDerivativeLogic contract is an interface that every syntheticId should implement
+contract IDerivativeLogic is LibDerivative {
+    /// @notice Validates ticker
+    /// @param _derivative Derivative Instance of derivative to validate
+    /// @return Returns boolean whether ticker is valid
+    function validateInput(Derivative memory _derivative) public view returns (bool);
+
+    /// @notice Calculates margin required for derivative creation
+    /// @param _derivative Derivative Instance of derivative
+    /// @return buyerMargin uint256 Margin needed from buyer (LONG position)
+    /// @return sellerMargin uint256 Margin needed from seller (SHORT position)
+    function getMargin(Derivative memory _derivative) public view returns (uint256 buyerMargin, uint256 sellerMargin);
+
+    /// @notice Calculates payout for derivative execution
+    /// @param _derivative Derivative Instance of derivative
+    /// @param _result uint256 Data retrieved from oracleId on the maturity
+    /// @return buyerPayout uint256 Payout in ratio for buyer (LONG position holder)
+    /// @return sellerPayout uint256 Payout in ratio for seller (SHORT position holder)
+    function getExecutionPayout(Derivative memory _derivative, uint256 _result)	public view returns (uint256 buyerPayout, uint256 sellerPayout);
+
+    /// @notice Returns syntheticId author address for Opium commissions
+    /// @return authorAddress address The address of syntheticId address
+    function getAuthorAddress() public view returns (address authorAddress);
+
+    /// @notice Returns syntheticId author commission in base of COMMISSION_BASE
+    /// @return commission uint256 Author commission
+    function getAuthorCommission() public view returns (uint256 commission);
+
+    /// @notice Returns whether thirdparty could execute on derivative's owner's behalf
+    /// @param _derivativeOwner address Derivative owner address
+    /// @return Returns boolean whether _derivativeOwner allowed third party execution
+    function thirdpartyExecutionAllowed(address _derivativeOwner) public view returns (bool);
+
+    /// @notice Returns whether syntheticId implements pool logic
+    /// @return Returns whether syntheticId implements pool logic
+    function isPool() public view returns (bool);
+
+    /// @notice Sets whether thirds parties are allowed or not to execute derivative's on msg.sender's behalf
+    /// @param _allow bool Flag for execution allowance
+    function allowThirdpartyExecution(bool _allow) public;
+
+    // Event with syntheticId metadata JSON string (for DIB.ONE derivative explorer)
+    event MetadataSet(string metadata);
+}
+
+// File: contracts/Helpers/PayoutHelper.sol
+
+pragma solidity 0.5.16;
+
+
+contract PayoutHelper is LibDerivative {
+    struct ExecutionPayout {
+       uint256 buyerPayout;
+       uint256 sellerPayout;
+    }
+
+    function getExecutionPayouts(Derivative memory _derivative, uint256[] memory _results)	public view returns (ExecutionPayout[] memory executionPayouts) {
+        IDerivativeLogic logic = IDerivativeLogic(_derivative.syntheticId);
+
+        executionPayouts = new ExecutionPayout[](_results.length);
+
+        for (uint256 i = 0; i < _results.length; i++) {
+            (uint256 buyerPayout, uint256 sellerPayout) = logic.getExecutionPayout(_derivative, _results[i]);
+            executionPayouts[i] = ExecutionPayout(
+                buyerPayout,
+                sellerPayout
+            );
+        }
+    }
+}
