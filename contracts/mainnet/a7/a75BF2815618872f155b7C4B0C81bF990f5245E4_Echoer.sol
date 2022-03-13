938927a97df6025d796c9cc0dd8498fbfcd8866f
minion <minion>
2022-03-13 11:13:06 +0000

    with 💕

---
+++ contracts/mainnet/a7/a75BF2815618872f155b7C4B0C81bF990f5245E4_Echoer.sol
@@ -0,0 +1,24 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-03-13
+*/
+
+// SPDX-License-Identifier: MIT
+// https://tornado.cash
+/*
+* d888888P                                           dP              a88888b.                   dP
+*    88                                              88             d8'   `88                   88
+*    88    .d8888b. 88d888b. 88d888b. .d8888b. .d888b88 .d8888b.    88        .d8888b. .d8888b. 88d888b.
+*    88    88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88    88        88'  `88 Y8ooooo. 88'  `88
+*    88    88.  .88 88       88    88 88.  .88 88.  .88 88.  .88 dP Y8.   .88 88.  .88       88 88    88
+*    dP    `88888P' dP       dP    dP `88888P8 `88888P8 `88888P' 88  Y88888P' `88888P8 `88888P' dP    dP
+* ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+*/
+pragma solidity ^0.6.0;
+
+contract Echoer {
+  event Echo(address indexed who, bytes data);
+
+  function echo(bytes calldata _data) external {
+    emit Echo(msg.sender, _data);
+  }
+}
