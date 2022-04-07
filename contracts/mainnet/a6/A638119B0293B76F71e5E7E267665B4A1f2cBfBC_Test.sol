3feb6f6a366bd5e35cb7101bcb78b066068bbd54
minion <minion>
2022-04-07 21:10:30 +0000

    with 💕

---
+++ contracts/mainnet/a6/A638119B0293B76F71e5E7E267665B4A1f2cBfBC_Test.sol
@@ -0,0 +1,20 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-04-07
+*/
+
+// Verified using https://dapp.tools
+
+// hevm: flattened sources of src/Test.sol
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity =0.8.11;
+pragma experimental ABIEncoderV2;
+
+////// src/Test.sol
+/* pragma solidity 0.8.11; */
+/* pragma experimental ABIEncoderV2; */
+
+contract Test {
+    function test() external pure returns (uint256) {
+        return 42;
+    }
+}
