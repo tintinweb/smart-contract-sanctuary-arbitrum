d13a2c78427a149b95d60e5231216bbbbdba8c72
minion <minion>
2022-05-13 21:22:15 +0000

    with 💕

---
+++ contracts/mainnet/c1/c141258d176df9bfa6a745afb321c3dcf72e0306_setNumberContract.sol
@@ -0,0 +1,14 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-05-13
+*/
+
+pragma solidity ^0.8.0;
+
+contract setNumberContract{
+    address reserved;
+    uint256 public number;
+    
+    function setNumber(uint256 _number) public {
+        number = _number + 1;
+    }
+}
