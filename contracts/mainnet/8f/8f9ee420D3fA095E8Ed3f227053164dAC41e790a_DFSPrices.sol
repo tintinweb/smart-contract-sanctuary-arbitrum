f2b417507205b0d45d77676f0555864ad5775dfc
minion <minion>
2022-05-23 21:12:31 +0000

    with 💕

---
+++ contracts/mainnet/8f/8f9ee420D3fA095E8Ed3f227053164dAC41e790a_DFSPrices.sol
@@ -0,0 +1,428 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-05-23
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.10;
+
+
+
+
+
+interface IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint256 digits);
+    function totalSupply() external view returns (uint256 supply);
+
+    function balanceOf(address _owner) external view returns (uint256 balance);
+
+    function transfer(address _to, uint256 _value) external returns (bool success);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
+
+    function approve(address _spender, uint256 _value) external returns (bool success);
+
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+
+
+
+library Address {
+    //insufficient balance
+    error InsufficientBalance(uint256 available, uint256 required);
+    //unable to send value, recipient may have reverted
+    error SendingValueFail();
+    //insufficient balance for call
+    error InsufficientBalanceForCall(uint256 available, uint256 required);
+    //call to non-contract
+    error NonContractCall();
+    
+    function isContract(address account) internal view returns (bool) {
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            codehash := extcodehash(account)
+        }
+        return (codehash != accountHash && codehash != 0x0);
+    }
+
+    function sendValue(address payable recipient, uint256 amount) internal {
+        uint256 balance = address(this).balance;
+        if (balance < amount){
+            revert InsufficientBalance(balance, amount);
+        }
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call{value: amount}("");
+        if (!(success)){
+            revert SendingValueFail();
+        }
+    }
+
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+        return functionCall(target, data, "Address: low-level call failed");
+    }
+
+    function functionCall(
+        address target,
+        bytes memory data,
+        string memory errorMessage
+    ) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    function functionCallWithValue(
+        address target,
+        bytes memory data,
+        uint256 value
+    ) internal returns (bytes memory) {
+        return
+            functionCallWithValue(target, data, value, "Address: low-level call with value failed");
+    }
+
+    function functionCallWithValue(
+        address target,
+        bytes memory data,
+        uint256 value,
+        string memory errorMessage
+    ) internal returns (bytes memory) {
+        uint256 balance = address(this).balance;
+        if (balance < value){
+            revert InsufficientBalanceForCall(balance, value);
+        }
+        return _functionCallWithValue(target, data, value, errorMessage);
+    }
+
+    function _functionCallWithValue(
+        address target,
+        bytes memory data,
+        uint256 weiValue,
+        string memory errorMessage
+    ) private returns (bytes memory) {
+        if (!(isContract(target))){
+            revert NonContractCall();
+        }
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{value: weiValue}(data);
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+
+
+
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+
+
+
+
+
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.transferFrom.selector, from, to, value)
+        );
+    }
+
+    /// @dev Edited so it always first approves 0 and then the value, because of non standard tokens
+    function safeApprove(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, 0));
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.approve.selector, spender, newAllowance)
+        );
+    }
+
+    function safeDecreaseAllowance(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(
+            value,
+            "SafeERC20: decreased allowance below zero"
+        );
+        _callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.approve.selector, spender, newAllowance)
+        );
+    }
+
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        bytes memory returndata = address(token).functionCall(
+            data,
+            "SafeERC20: low-level call failed"
+        );
+        if (returndata.length > 0) {
+            // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+
+
+
+
+contract ArbitrumExchangeAddresses {
+
+    address internal constant FEE_RECIPIENT_ADDRESS = 0xe000e3c9428D539566259cCd89ed5fb85e655A01;
+    address internal constant DISCOUNT_ADDRESS = 0x9b8F21A50511B6Ffb304190072908A3756A9dc87;
+    address internal constant SAVER_EXCHANGE_REGISTRY = 0xaB1E4b72BC2f3890F052df111EE626c1c7229F26;
+    address internal constant ZRX_ALLOWLIST_ADDR = 0x5eD8e74b1caE57B0c68B3278B88589991FBa0750;
+    address internal constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant GROUP_REGISTRY = 0xb03fe103f54841821C080C124312059c9A3a7B5c;
+}
+
+
+
+
+
+contract ExchangeHelper is ArbitrumExchangeAddresses {
+}
+
+
+
+
+
+
+contract DFSPrices is ExchangeHelper {
+
+    enum ActionType { SELL, BUY }
+    
+    error OutOfRangeSlicingError();
+
+    /// @notice Returns the best estimated price from 2 exchanges
+    /// @param _amount Amount of source tokens you want to exchange
+    /// @param _srcToken Address of the source token
+    /// @param _destToken Address of the destination token
+    /// @param _type Type of action SELL|BUY
+    /// @param _wrappers Array of wrapper addresses to compare
+    /// @return (address, uint) The address of the best exchange and the exchange price
+    function getBestPrice(
+        uint256 _amount,
+        address _srcToken,
+        address _destToken,
+        ActionType _type,
+        address[] memory _wrappers,
+        bytes[] memory _additionalData
+    ) public returns (address, uint256) {
+
+        uint256[] memory rates = new uint256[](_wrappers.length);
+        for (uint i=0; i<_wrappers.length; i++) {
+            rates[i] = getExpectedRate(_wrappers[i], _srcToken, _destToken, _amount, _type, _additionalData[i]);
+        }
+
+        return getBiggestRate(_wrappers, rates);
+    }
+
+    /// @notice Return the expected rate from the exchange wrapper
+    /// @dev In case of Oasis/Uniswap handles the different precision tokens
+    /// @param _wrapper Address of exchange wrapper
+    /// @param _srcToken From token
+    /// @param _destToken To token
+    /// @param _amount Amount to be exchanged
+    /// @param _type Type of action SELL|BUY
+    function getExpectedRate(
+        address _wrapper,
+        address _srcToken,
+        address _destToken,
+        uint256 _amount,
+        ActionType _type,
+        bytes memory _additionalData
+    ) public returns (uint256) {
+        bool success;
+        bytes memory result;
+
+        if (_type == ActionType.SELL) {
+            (success, result) = _wrapper.call(abi.encodeWithSignature(
+                "getSellRate(address,address,uint256,bytes)",
+                _srcToken,
+                _destToken,
+                _amount,
+                _additionalData
+            ));
+
+        } else {
+            (success, result) = _wrapper.call(abi.encodeWithSignature(
+                "getBuyRate(address,address,uint256,bytes)",
+                _srcToken,
+                _destToken,
+                _amount,
+                _additionalData
+            ));
+        }
+
+        if (success) {
+            return sliceUint(result, 0);
+        }
+
+        return 0;
+    }
+
+    /// @notice Finds the biggest rate between exchanges, needed for sell rate
+    /// @param _wrappers Array of wrappers to compare
+    /// @param _rates Array of rates to compare
+    function getBiggestRate(
+        address[] memory _wrappers,
+        uint256[] memory _rates
+    ) internal pure returns (address, uint) {
+        uint256 maxIndex = 0;
+
+        // starting from 0 in case there is only one rate in array
+        for (uint256 i=0; i<_rates.length; i++) {
+            if (_rates[i] > _rates[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        return (_wrappers[maxIndex], _rates[maxIndex]);
+    }
+
+    function getDecimals(address _token) internal view returns (uint256) {
+        if (_token == ETH_ADDRESS) return 18;
+
+        return IERC20(_token).decimals();
+    }
+
+    function sliceUint(bytes memory bs, uint256 start) internal pure returns (uint256) {
+        if (bs.length < start + 32){
+            revert OutOfRangeSlicingError();
+        }
+
+        uint256 x;
+        assembly {
+            x := mload(add(bs, add(0x20, start)))
+        }
+
+        return x;
+    }
+}
