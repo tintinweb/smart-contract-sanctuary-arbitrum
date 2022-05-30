9bde3f2d8aeb1e1d54d8991a1838ba8321624665
minion <minion>
2022-05-30 21:06:22 +0000

    with 💕

---
+++ contracts/mainnet/66/6683bea417503777a740e7ed2119561cb7002131_HelloArbitrum.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-05-30
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+contract HelloArbitrum {
+    function getGreeting() public pure returns(string memory){
+        return "Hello Arbitrum";
+    }
+}
