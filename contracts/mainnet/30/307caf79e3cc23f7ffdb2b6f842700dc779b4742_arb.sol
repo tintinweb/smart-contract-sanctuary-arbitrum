03f6a0ee2b879a430b63b48f5e58db983757ec2b
minion <minion>
2022-06-16 21:07:41 +0000

    with 💕

---
+++ contracts/mainnet/30/307caf79e3cc23f7ffdb2b6f842700dc779b4742_arb.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-06-16
+*/
+
+// Tes
+pragma solidity >=0.5.0 <0.7.0;
+contract arb {
+    function gm()public pure returns (string memory){
+        return 'arb den';
+    }
+
+}
