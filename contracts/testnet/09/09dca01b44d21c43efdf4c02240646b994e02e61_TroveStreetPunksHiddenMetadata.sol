124e98291732e944d369a9f2e4e755619ebc0844
minion <minion>
2022-06-17 07:15:22 +0000

    with 💕

---
+++ contracts/testnet/09/09dca01b44d21c43efdf4c02240646b994e02e61_TroveStreetPunksHiddenMetadata.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "./ITroveStreetPunksMetadata.sol";
+
+contract TroveStreetPunksHiddenMetadata is ITroveStreetPunksMetadata {
+
+    function metadataOf(uint256) external pure override returns (string memory) {
+        return "ipfs://Qma57uaWE17Mh6qSDu6gbXM7A9H4oJa68iCpzDHykG87jN";
+    }
+
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+interface ITroveStreetPunksMetadata {
+
+    function metadataOf(uint256 tokenId) external view returns (string memory);
+
+}
