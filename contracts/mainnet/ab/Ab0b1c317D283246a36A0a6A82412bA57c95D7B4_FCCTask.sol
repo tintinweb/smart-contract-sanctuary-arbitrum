9564d773594ab0929e24e49f28ca6f254f3ba87d
minion <minion>
2022-07-19 21:22:46 +0000

    with 💕

---
+++ contracts/mainnet/ab/Ab0b1c317D283246a36A0a6A82412bA57c95D7B4_FCCTask.sol
@@ -0,0 +1,23 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-07-19
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FCCTask {
+    uint256 public s_variable = 0;
+    uint256 public s_otherVar = 0;
+
+    function doSomething() public {
+        s_variable = 123;
+    }
+
+    function doSomethingElse() public {
+        s_otherVar = s_otherVar + 1;
+    }
+
+    function getSelector() public pure returns (bytes4 selector) {
+        selector = bytes4(keccak256(bytes("doSomethingElse()")));
+    }
+}
