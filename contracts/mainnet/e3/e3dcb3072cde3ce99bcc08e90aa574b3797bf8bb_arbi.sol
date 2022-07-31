28f947a2fd4800c2456fae7f6bb1426340d8f078
minion <minion>
2022-07-31 11:05:22 +0000

    with 💕

---
+++ contracts/mainnet/e3/e3dcb3072cde3ce99bcc08e90aa574b3797bf8bb_arbi.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-07-31
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+contract arbi {
+    function get()public pure returns (string memory){
+        return 'gM arbi';
+    }
+}
