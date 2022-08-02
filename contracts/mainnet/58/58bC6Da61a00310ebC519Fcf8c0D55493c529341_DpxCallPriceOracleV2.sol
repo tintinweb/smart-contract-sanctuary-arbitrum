0511017c180808a15b1eccab382de0e925fc2a90
minion <minion>
2022-08-02 11:04:55 +0000

    with 💕

---
+++ contracts/mainnet/58/58bC6Da61a00310ebC519Fcf8c0D55493c529341_DpxCallPriceOracleV2.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface AggregatorInterface {
+  function latestAnswer() external view returns (int256);
+
+  function latestTimestamp() external view returns (uint256);
+
+  function latestRound() external view returns (uint256);
+
+  function getAnswer(uint256 roundId) external view returns (int256);
+
+  function getTimestamp(uint256 roundId) external view returns (uint256);
+
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AggregatorInterface.sol";
+import "./AggregatorV3Interface.sol";
+
+interface AggregatorV2V3Interface is AggregatorInterface, AggregatorV3Interface {}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  // getRoundData and latestRoundData should both raise "No data present"
+  // if they do not have data to report, instead of returning unset values
+  // which could be misinterpreted as actual reported values.
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
+
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface IPriceOracle {
+    function getCollateralPrice() external view returns (uint256);
+
+    function getUnderlyingPrice() external view returns (uint256);
+}
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Interfaces
+import {AggregatorV2V3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
+import {IPriceOracle} from "../../../interfaces/IPriceOracle.sol";
+
+contract DpxCallPriceOracleV2 is IPriceOracle {
+    AggregatorV2V3Interface internal priceFeed;
+    AggregatorV2V3Interface internal sequencerUptimeFeed;
+
+    uint256 public constant GRACE_PERIOD_TIME = 3600;
+
+    error SequencerDown();
+    error GracePeriodNotOver();
+    error HeartbeatNotFulfilled();
+
+    /**
+     * Network: Arbitrum Mainnet
+     * Data Feed: DPX/USD
+     * Data Feed Proxy Address: 0xc373B9DB0707fD451Bc56bA5E9b029ba26629DF0
+     * Sequencer Uptime Proxy Address: 0xFdB631F5EE196F0ed6FAa767959853A9F217697D
+     */
+    constructor() {
+        priceFeed = AggregatorV2V3Interface(
+            0xc373B9DB0707fD451Bc56bA5E9b029ba26629DF0
+        );
+        sequencerUptimeFeed = AggregatorV2V3Interface(
+            0xFdB631F5EE196F0ed6FAa767959853A9F217697D
+        );
+    }
+
+    /// @notice Returns the collateral price
+    function getCollateralPrice() external view returns (uint256) {
+        return getUnderlyingPrice();
+    }
+
+    /// @notice Returns the underlying price
+    function getUnderlyingPrice() public view returns (uint256) {
+        (, int256 answer, uint256 startedAt, , ) = sequencerUptimeFeed
+            .latestRoundData();
+
+        // Answer == 0: Sequencer is up
+        // Answer == 1: Sequencer is down
+        bool isSequencerUp = answer == 0;
+        if (!isSequencerUp) {
+            revert SequencerDown();
+        }
+
+        // Make sure the grace period has passed after the sequencer is back up.
+        uint256 timeSinceUp = block.timestamp - startedAt;
+        if (timeSinceUp <= GRACE_PERIOD_TIME) {
+            revert GracePeriodNotOver();
+        }
+
+        (, int256 price, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+
+        if ((block.timestamp - updatedAt) > 86400) {
+            revert HeartbeatNotFulfilled();
+        }
+
+        return uint256(price);
+    }
+}
---
+++ contracts/mainnet/9e/9E250cAe25Cc0fD6e955285Fe981b62c4479176b_DpxPutPriceOracleV2.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface AggregatorInterface {
+  function latestAnswer() external view returns (int256);
+
+  function latestTimestamp() external view returns (uint256);
+
+  function latestRound() external view returns (uint256);
+
+  function getAnswer(uint256 roundId) external view returns (int256);
+
+  function getTimestamp(uint256 roundId) external view returns (uint256);
+
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AggregatorInterface.sol";
+import "./AggregatorV3Interface.sol";
+
+interface AggregatorV2V3Interface is AggregatorInterface, AggregatorV3Interface {}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  // getRoundData and latestRoundData should both raise "No data present"
+  // if they do not have data to report, instead of returning unset values
+  // which could be misinterpreted as actual reported values.
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {IERC20} from "./IERC20.sol";
+
+interface ICrv2Pool is IERC20 {
+    function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount)
+        external
+        returns (uint256);
+
+    function remove_liquidity_one_coin(
+        uint256 _token_amount,
+        int128 i,
+        uint256 min_amount
+    ) external returns (uint256);
+
+    function get_virtual_price() external view returns (uint256);
+}
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ * NOTE: Modified to include symbols and decimals.
+ */
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function symbol() external view returns (string memory);
+
+  function decimals() external view returns (uint8);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  function allowance(address owner, address spender)
+    external
+    view
+    returns (uint256);
+
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface IPriceOracle {
+    function getCollateralPrice() external view returns (uint256);
+
+    function getUnderlyingPrice() external view returns (uint256);
+}
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Interfaces
+import {AggregatorV2V3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
+import {IPriceOracle} from "../../../interfaces/IPriceOracle.sol";
+import {ICrv2Pool} from "../../../external/interfaces/ICrv2Pool.sol";
+
+contract DpxPutPriceOracleV2 is IPriceOracle {
+    ICrv2Pool public constant CRV_2POOL =
+        ICrv2Pool(0x7f90122BF0700F9E7e1F688fe926940E8839F353);
+
+    AggregatorV2V3Interface internal priceFeed;
+    AggregatorV2V3Interface internal sequencerUptimeFeed;
+
+    uint256 public constant GRACE_PERIOD_TIME = 3600;
+
+    error SequencerDown();
+    error GracePeriodNotOver();
+    error HeartbeatNotFulfilled();
+
+    /**
+     * Network: Arbitrum Mainnet
+     * Data Feed: DPX/USD
+     * Data Feed Proxy Address: 0xc373B9DB0707fD451Bc56bA5E9b029ba26629DF0
+     * Sequencer Uptime Proxy Address: 0xFdB631F5EE196F0ed6FAa767959853A9F217697D
+     */
+    constructor() {
+        priceFeed = AggregatorV2V3Interface(
+            0xc373B9DB0707fD451Bc56bA5E9b029ba26629DF0
+        );
+        sequencerUptimeFeed = AggregatorV2V3Interface(
+            0xFdB631F5EE196F0ed6FAa767959853A9F217697D
+        );
+    }
+
+    /// @notice Returns the collateral price
+    function getCollateralPrice() external view returns (uint256) {
+        return CRV_2POOL.get_virtual_price() / 1e10;
+    }
+
+    /// @notice Returns the underlying price
+    function getUnderlyingPrice() external view returns (uint256) {
+        (, int256 answer, uint256 startedAt, , ) = sequencerUptimeFeed
+            .latestRoundData();
+
+        // Answer == 0: Sequencer is up
+        // Answer == 1: Sequencer is down
+        bool isSequencerUp = answer == 0;
+        if (!isSequencerUp) {
+            revert SequencerDown();
+        }
+
+        // Make sure the grace period has passed after the sequencer is back up.
+        uint256 timeSinceUp = block.timestamp - startedAt;
+        if (timeSinceUp <= GRACE_PERIOD_TIME) {
+            revert GracePeriodNotOver();
+        }
+
+        (, int256 price, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+
+        if ((block.timestamp - updatedAt) > 86400) {
+            revert HeartbeatNotFulfilled();
+        }
+
+        return uint256(price);
+    }
+}
