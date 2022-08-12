951a8ebe2fe38d5548f3b671b0a6f7dfe23d4687
minion <minion>
2022-08-12 07:21:00 +0000

    with 💕

---
+++ contracts/testnet/94/940a6d75d5c3f65eca476cff369fed235265749a_Simple.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.12;
+
+contract Simple {
+    function greet() external pure returns (string memory) {
+        return "Hello";
+    }
+}
