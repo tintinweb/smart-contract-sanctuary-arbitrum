3cb5ee763a585550f3346ee0f02266aa9737fa28
minion <minion>
2022-08-28 17:04:51 +0000

    with 💕

---
+++ contracts/mainnet/fb/Fb7b4c7f7E6D10CdB97Ff5358a8107c92bd78Bc8_MerkleClaim2.sol
@@ -0,0 +1,406 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+/*//////////////////////////////////////////////////////////////
+                            Imports
+//////////////////////////////////////////////////////////////*/
+
+import {IERC20} from "./lib/IERC20.sol";
+import {IERC721} from "./lib/IERC721.sol";
+import {MerkleProof} from "./lib/MerkleProof.sol";
+
+
+/*//////////////////////////////////////////////////////////////
+                           Interfaces
+//////////////////////////////////////////////////////////////*/
+
+interface MerkleClaim {
+    function endClaim() external view returns (uint256);
+    function hasClaimed(address account) external view returns (bool);
+}
+
+/*//////////////////////////////////////////////////////////////
+                            Contract
+//////////////////////////////////////////////////////////////*/
+
+/// @title MerkleClaimERC20
+/// @notice tokens claimable by members of a merkle tree
+contract MerkleClaim2 {
+
+    /*//////////////////////////////////////////////////////////////
+                            Immutable storage
+    //////////////////////////////////////////////////////////////*/
+
+    address public admin = 0x5f49174FdEb42959f3234053b18F5c4ad497CC55;
+    address public NFT = 0x64b34AD4c1bb4BFbF4c43B9b82aB245C3D58a1Bf;
+
+    uint256 public endClaim;
+    uint256 public tier3amount;
+    uint256 public lastId;
+    /// @notice ERC20-claimee inclusion root
+    bytes32 public merkleRoot=0xfd3d146001235eb7f1360b6a7e73b21ede3db4e45e6d008df917819a3b0389c8;
+
+    /*//////////////////////////////////////////////////////////////
+                            Mutable storage
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Mapping of addresses who have claimed tokens
+    mapping(address => bool) public hasClaimed;
+
+    /*//////////////////////////////////////////////////////////////
+                               Modifiers
+    //////////////////////////////////////////////////////////////*/
+
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+    
+    /*//////////////////////////////////////////////////////////////
+                                 Errors
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Thrown if address has already claimed
+    error AlreadyClaimed();
+    /// @notice Thrown if address/amount are not part of Merkle tree
+    error NotInMerkle();
+
+    /*//////////////////////////////////////////////////////////////
+                              Constructor
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() {
+
+        lastId = 134;
+        endClaim = MerkleClaim(NFT).endClaim();
+
+        tier3amount = 1 * (10 ** 6);
+    
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                Events
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted after a successful token claim
+    /// @param to recipient of claim
+    /// @param amount of tokens claimed
+    event Claim(address indexed to, uint256 amount);
+
+    /// @notice Emitted after admin withdraws remaining funds
+    /// @param amount of tokens withdrawn
+    event Withdraw(uint256 amount);
+    
+
+    /*//////////////////////////////////////////////////////////////
+                            Claim Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Allows claiming tokens if address is part of merkle tree
+    /// @param to address of claimee
+    /// @param amount of tokens owed to claimee
+    /// @param proof merkle proof to prove address and amount are in tree
+    function claim(address to, uint256 amount, bytes32[] calldata proof) external {
+        
+        // Throw if address has already claimed tokens
+        if (hasClaimed[to]) revert AlreadyClaimed();
+        if (MerkleClaim(NFT).hasClaimed(to)) revert AlreadyClaimed();
+        // Throw if timestamp not in the claim window
+        require(block.timestamp < endClaim, "Claim has expired");
+        // check amount validity
+        require(amount == tier3amount, "Amount not valid");
+
+        // Verify merkle proof, or revert if not in tree
+        bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+        bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+        if (!isValidLeaf) revert NotInMerkle();
+
+        // Set address to claimed
+        hasClaimed[to] = true;
+        // mint tier 3
+        (bool success) = _transfer(to);
+        require(success, "Could not mint token");
+
+        // Emit claim event
+        emit Claim(to, amount);
+    }
+
+    function _transfer(address to) internal returns (bool) {
+        
+        require(to != address(0x0), "Invalid address");
+        require(lastId < 219, "No more tokens to mint");
+
+        unchecked{lastId++;}
+        IERC721(NFT).transferFrom(
+            address(this),
+            to,
+            lastId
+        );
+        return true;
+
+    }
+
+    /// @notice Send a batch of NFTs at once.
+    /// @notice MUST call setApprovalForAll on the NFT contract first
+    function batchSend(uint first, uint last) public onlyAdmin {
+
+        for (uint i = first; i < last; i++) {
+            IERC721(NFT).transferFrom(
+                admin,
+                address(this),
+                i
+            );
+        }
+
+    }
+
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+interface IERC20 {
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+    function decimals() external view returns (uint8);
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/IERC721.sol)
+
+pragma solidity ^0.8.0;
+
+import "./IERC165.sol";
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC721 is IERC165 {
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must have been allowed to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    /**
+     * @dev Transfers `tokenId` token from `from` to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    /**
+     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
+     * The approval is cleared when the token is transferred.
+     *
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
+     *
+     * Requirements:
+     *
+     * - The caller must own the token or be an approved operator.
+     * - `tokenId` must exist.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address to, uint256 tokenId) external;
+
+    /**
+     * @dev Approve or remove `operator` as an operator for the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
+     *
+     * Requirements:
+     *
+     * - The `operator` cannot be the caller.
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setApprovalForAll(address operator, bool _approved) external;
+
+    /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    /**
+     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
+     *
+     * See {setApprovalForAll}
+     */
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+/**
+ * @dev These functions deal with verification of Merkle Trees proofs.
+ *
+ * The proofs can be generated using the JavaScript library
+ * https://github.com/miguelmota/merkletreejs[merkletreejs].
+ * Note: the hashing algorithm should be keccak256 and pair sorting should be enabled.
+ *
+ * See `test/utils/cryptography/MerkleProof.test.js` for some examples.
+ */
+library MerkleProof {
+    /**
+     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, a `proof` must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Each
+     * pair of leaves and each pair of pre-images are assumed to be sorted.
+     */
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
+        return processProof(proof, leaf) == root;
+    }
+
+    /**
+     * @dev Returns the rebuilt hash obtained by traversing a Merklee tree up
+     * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
+     * hash matches the root of the tree. When processing the proof, the pairs
+     * of leafs & pre-images are assumed to be sorted.
+     *
+     * _Available since v4.4._
+     */
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+            if (computedHash <= proofElement) {
+                // Hash(current computed hash + current element of the proof)
+                computedHash = _efficientHash(computedHash, proofElement);
+            } else {
+                // Hash(current element of the proof + current computed hash)
+                computedHash = _efficientHash(proofElement, computedHash);
+            }
+        }
+        return computedHash;
+    }
+
+    function _efficientHash(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
+        assembly {
+            mstore(0x00, a)
+            mstore(0x20, b)
+            value := keccak256(0x00, 0x40)
+        }
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
