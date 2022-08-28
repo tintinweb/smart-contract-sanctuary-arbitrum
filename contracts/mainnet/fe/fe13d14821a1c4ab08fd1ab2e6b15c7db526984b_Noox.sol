257335a373c7f9c68f5d340ec917d13101d4314a
minion <minion>
2022-08-28 11:02:39 +0000

    with 💕

---
+++ contracts/mainnet/fe/fe13d14821a1c4ab08fd1ab2e6b15c7db526984b_Noox.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-08-28
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Noox {
+    string public greet = "Noox is great";
+}
