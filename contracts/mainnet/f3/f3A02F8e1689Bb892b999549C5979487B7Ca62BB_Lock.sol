296b3fd218176d50f199023790f73cdb76d9bd06
minion <minion>
2022-09-11 17:20:03 +0000

    with 💕

---
+++ contracts/mainnet/f3/f3A02F8e1689Bb892b999549C5979487B7Ca62BB_Lock.sol
@@ -0,0 +1,27 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-09-11
+*/
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lock {
+
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+    
+}
