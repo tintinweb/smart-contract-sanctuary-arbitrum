4393b048d3440aa6eb60fae6f6397be605f74ad6
minion <minion>
2022-09-20 07:28:03 +0000

    with 💕

---
+++ contracts/testnet/8a/8a8D9dEBD77cD5058c8923e1cde6428219C47eeD_CoinFlip.sol
@@ -0,0 +1,308 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-09-19
+*/
+
+// SPDX-License-Identifier: MIT 
+pragma solidity >=0.8.5 <0.9.0; 
+ 
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+ 
+contract CoinFlip { 
+ 
+    using SafeMath for uint256; 
+    uint256 public totalWins; 
+    uint256 public totalLosses; 
+    uint256 public totalFees; 
+    uint256 public totalNetAmount; 
+    address public owner;
+    uint256 lastHash; 
+    uint256 FACTOR = 57896044618658097711785492874343953926634992332820282019728792003956564819968; 
+ 
+    constructor() { 
+        totalWins = 0; 
+        totalLosses = 0; 
+        totalFees = 0; 
+        totalNetAmount = 0; 
+        owner = msg.sender;
+    } 
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+ 
+    function flip(bool _guess) payable public returns (bool) { 
+ 
+        if(msg.value < 0.0001 ether) { 
+            revert("Bet must be higher than 0.0001 ETH!"); 
+        } 
+
+        if(msg.value > 0.1 ether) { 
+            revert("Bet must be lower or equal to 0.1 ETH!"); 
+        } 
+ 
+        if(msg.value > address(this).balance) { 
+            revert("Bet higher than contract balance!"); 
+        } 
+         
+        uint256 blockValue = uint256(blockhash(block.number.sub(1))); 
+ 
+        if (lastHash == blockValue) { 
+            revert(); 
+        } 
+ 
+        lastHash = blockValue; 
+        uint256 coinFlip = blockValue.div(FACTOR); 
+        bool side = coinFlip == 1 ? true : false; 
+ 
+        if (side == _guess) { 
+ 
+            // Calculate win 
+            uint256 fee = (msg.value / 100) * 3; 
+            uint256 win = (msg.value * 2) - fee; 
+ 
+            // Pay win 
+            payable(msg.sender).transfer(win); 
+             
+            // Increment total wins + total fees + total net amount 
+            totalWins += win; 
+            totalFees += fee; 
+            totalNetAmount -= win; 
+ 
+            return true; 
+ 
+        } else { 
+            totalLosses += msg.value; 
+            totalNetAmount += msg.value; 
+            return false; 
+        } 
+    } 
+ 
+   
+    function balance() public view returns (uint256) { 
+        // Get total contract balance 
+        return address(this).balance; 
+    } 
+ 
+    function deposit() payable public returns (bool) { 
+        return true; 
+    } 
+
+    function withdraw(uint256 _amount) public onlyOwner returns (bool){
+        // Let owner withdraw funds
+        address payable sendTo = payable(address(owner));
+        sendTo.transfer(_amount);
+        return true;
+    } 
+}
