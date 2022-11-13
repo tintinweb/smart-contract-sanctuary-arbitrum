2f8bf89b58b203f009a511c2ca0bb2239b3bfe8b
minion <minion>
2022-11-13 17:13:28 +0000

    with 💕

---
+++ contracts/mainnet/ea/EA9CB63810bDb021480195f0c68dbC6E34CA76B6_VariantDispenser.sol
@@ -0,0 +1,483 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+import "./Adventurer/IAdventurerData.sol";
+import "./Adventurer/IAovLegacy.sol";
+
+import "./Manager/ManagerModifier.sol";
+
+contract VariantDispenser is Pausable, ManagerModifier {
+  //=======================================
+  // Immutables
+  //=======================================
+  IAdventurerData public immutable ADVENTURER_DATA;
+  IAovLegacy public immutable LEGACY;
+
+  //=======================================
+  // Events
+  //=======================================
+  event Dispensed(address addr, uint256 id, uint256 archetypeId);
+
+  //=======================================
+  // Constructor
+  //=======================================
+  constructor(
+    address _manager,
+    address _adventurerData,
+    address _legacy
+  ) ManagerModifier(_manager) {
+    ADVENTURER_DATA = IAdventurerData(_adventurerData);
+    LEGACY = IAovLegacy(_legacy);
+  }
+
+  //=======================================
+  // External
+  //=======================================
+  function dispense(
+    address[] calldata _addresses,
+    uint256[] calldata _ids,
+    uint256[] calldata _archetypes
+  ) external whenNotPaused onlyAdmin {
+    for (uint256 j = 0; j < _addresses.length; j++) {
+      address addr = _addresses[j];
+      uint256 id = _ids[j];
+      uint256 archetypeId = _archetypes[j];
+
+      // Update legacy
+      LEGACY.chronicle(addr, id, _adventurerArchetype(addr, id), archetypeId);
+
+      // Update level
+      ADVENTURER_DATA.updateAov(addr, id, 1, archetypeId);
+
+      emit Dispensed(addr, id, archetypeId);
+    }
+  }
+
+  //=======================================
+  // Admin
+  //=======================================
+  function pause() external onlyAdmin {
+    _pause();
+  }
+
+  function unpause() external onlyAdmin {
+    _unpause();
+  }
+
+  //=======================================
+  // Internal
+  //=======================================
+  function _adventurerArchetype(address _addr, uint256 _adventurerId)
+    internal
+    view
+    returns (uint256)
+  {
+    return ADVENTURER_DATA.aov(_addr, _adventurerId, 1);
+  }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+interface IAovLegacy {
+  function chronicle(
+    address _addr,
+    uint256 _adventurerId,
+    uint256 _currentArchetype,
+    uint256 _archetype
+  ) external;
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+interface IAdventurerData {
+  function initData(
+    address[] calldata _addresses,
+    uint256[] calldata _ids,
+    bytes32[][] calldata _proofs,
+    uint256[] calldata _professions,
+    uint256[][] calldata _points
+  ) external;
+
+  function baseProperties(
+    address _addr,
+    uint256 _id,
+    uint256 _start,
+    uint256 _end
+  ) external view returns (uint256[] memory);
+
+  function aovProperties(
+    address _addr,
+    uint256 _id,
+    uint256 _start,
+    uint256 _end
+  ) external view returns (uint256[] memory);
+
+  function extensionProperties(
+    address _addr,
+    uint256 _id,
+    uint256 _start,
+    uint256 _end
+  ) external view returns (uint256[] memory);
+
+  function createFor(
+    address _addr,
+    uint256 _id,
+    uint256[] calldata _points
+  ) external;
+
+  function createFor(
+    address _addr,
+    uint256 _id,
+    uint256 _archetype
+  ) external;
+
+  function addToBase(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function updateBase(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function removeFromBase(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function addToAov(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function updateAov(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function removeFromAov(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function addToExtension(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function updateExtension(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function removeFromExtension(
+    address _addr,
+    uint256 _id,
+    uint256 _prop,
+    uint256 _val
+  ) external;
+
+  function base(
+    address _addr,
+    uint256 _id,
+    uint256 _prop
+  ) external view returns (uint256);
+
+  function aov(
+    address _addr,
+    uint256 _id,
+    uint256 _prop
+  ) external view returns (uint256);
+
+  function extension(
+    address _addr,
+    uint256 _id,
+    uint256 _prop
+  ) external view returns (uint256);
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "../Manager/IManager.sol";
+
+abstract contract ManagerModifier {
+  //=======================================
+  // Immutables
+  //=======================================
+  IManager public immutable MANAGER;
+
+  //=======================================
+  // Constructor
+  //=======================================
+  constructor(address _manager) {
+    MANAGER = IManager(_manager);
+  }
+
+  //=======================================
+  // Modifiers
+  //=======================================
+  modifier onlyAdmin() {
+    require(MANAGER.isAdmin(msg.sender), "Manager: Not an Admin");
+    _;
+  }
+
+  modifier onlyManager() {
+    require(MANAGER.isManager(msg.sender, 0), "Manager: Not manager");
+    _;
+  }
+
+  modifier onlyMinter() {
+    require(MANAGER.isManager(msg.sender, 1), "Manager: Not minter");
+    _;
+  }
+
+  modifier onlyTokenMinter() {
+    require(MANAGER.isManager(msg.sender, 2), "Manager: Not token minter");
+    _;
+  }
+
+  modifier onlyBinder() {
+    require(MANAGER.isManager(msg.sender, 3), "Manager: Not binder");
+    _;
+  }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (security/ReentrancyGuard.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (security/Pausable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which allows children to implement an emergency stop
+ * mechanism that can be triggered by an authorized account.
+ *
+ * This module is used through inheritance. It will make available the
+ * modifiers `whenNotPaused` and `whenPaused`, which can be applied to
+ * the functions of your contract. Note that they will not be pausable by
+ * simply including this module, only once the modifiers are put in place.
+ */
+abstract contract Pausable is Context {
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Paused(address account);
+
+    /**
+     * @dev Emitted when the pause is lifted by `account`.
+     */
+    event Unpaused(address account);
+
+    bool private _paused;
+
+    /**
+     * @dev Initializes the contract in unpaused state.
+     */
+    constructor() {
+        _paused = false;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is not paused.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    modifier whenNotPaused() {
+        _requireNotPaused();
+        _;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is paused.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    modifier whenPaused() {
+        _requirePaused();
+        _;
+    }
+
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
+    function paused() public view virtual returns (bool) {
+        return _paused;
+    }
+
+    /**
+     * @dev Throws if the contract is paused.
+     */
+    function _requireNotPaused() internal view virtual {
+        require(!paused(), "Pausable: paused");
+    }
+
+    /**
+     * @dev Throws if the contract is not paused.
+     */
+    function _requirePaused() internal view virtual {
+        require(paused(), "Pausable: not paused");
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    function _pause() internal virtual whenNotPaused {
+        _paused = true;
+        emit Paused(_msgSender());
+    }
+
+    /**
+     * @dev Returns to normal state.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    function _unpause() internal virtual whenPaused {
+        _paused = false;
+        emit Unpaused(_msgSender());
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+interface IManager {
+  function isAdmin(address _addr) external view returns (bool);
+
+  function isManager(address _addr, uint256 _type) external view returns (bool);
+
+  function addManager(address _addr, uint256 _type) external;
+
+  function removeManager(address _addr, uint256 _type) external;
+
+  function addAdmin(address _addr) external;
+
+  function removeAdmin(address _addr) external;
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
