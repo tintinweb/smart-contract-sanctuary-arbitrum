593c153474fb12feb886a742a13987edc1562d6e
minion <minion>
2022-11-25 21:06:06 +0000

    with 💕

---
+++ contracts/mainnet/df/DF8cB93Df06e92993D6479B098109b7b0b1304A9_HelloWorld.sol
@@ -0,0 +1,31 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-11-25
+*/
+
+// Specifies the version of Solidity, using semantic versioning.
+
+pragma solidity ^0.7.0;
+
+// Defines a contract named `HelloWorld`
+
+contract HelloWorld {
+
+   // Declares a state variable `message` of type `string`.
+
+   string public message;
+
+   // Constructors are used to initialize the contract's data.
+
+   constructor(string memory initMessage) {
+
+      // Accepts a string argument `initMessage`.
+
+      message = initMessage;
+   }
+
+   // A public function that accepts a string argument.
+
+   function update(string memory newMessage) public {
+      message = newMessage;
+   }
+}
