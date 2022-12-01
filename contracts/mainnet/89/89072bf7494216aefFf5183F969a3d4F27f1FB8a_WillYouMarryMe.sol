aa55c589f99a04c6d503420e23f82dfe35e04655
minion <minion>
2022-12-01 21:06:04 +0000

    with 💕

---
+++ contracts/mainnet/89/89072bf7494216aefFf5183F969a3d4F27f1FB8a_WillYouMarryMe.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.8.4;
+
+/**
+ * If you are reading this message...
+ * You are the one and only, and the loveliest person I’ve ever met.
+ * Ever since I met you, I have learned to be stronger, kinder, more passionate,
+ * and I hope I make you feel the same way.
+ * You have indeed seen the best and the worst of me, and yet you still love and believe in me.
+ * So this is what I propose - let this infinite truth machine be the witness of all the love I promise to give,
+ * and the rest of my life spent with you.
+ * Will you marry me?
+ **/
+
+contract WillYouMarryMe {
+    bool engaged = false;
+
+    function yesIdo() public {
+        engaged = true;
+    }
+}
