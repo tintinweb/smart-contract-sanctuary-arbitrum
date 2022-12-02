a6e8cdf2fc6cd316e0035142de40dd0ae1e2d25d
minion <minion>
2022-12-02 21:06:05 +0000

    with 💕

---
+++ contracts/mainnet/2b/2b402a9b3dc4e0539a106bbbb61a86ca2c709820_snoopfear4.sol
@@ -0,0 +1,28 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-12-02
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract snoopfear4 {
+
+    address public owner;
+    mapping (address => uint) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function Donatecash() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function MoneyBackcash() public {
+        address payable _to = payable(owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
