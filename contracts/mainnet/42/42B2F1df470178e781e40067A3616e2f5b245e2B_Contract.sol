fa15a25ae68a15c60e8d88348b346093cda91407
minion <minion>
2022-12-16 21:06:13 +0000

    with 💕

---
+++ contracts/mainnet/42/42B2F1df470178e781e40067A3616e2f5b245e2B_Contract.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at Arbiscan on 2022-12-16
+*/
+
+contract Contract{
+    uint256 public _totalSupply = 10 ** 9 * (10 ** 18); // 0.1..T
+    uint256 public maxTxAmount = _totalSupply / 200;
+    uint256 public maxWalletToken = _totalSupply / 50;
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowances;
+
+    mapping (address => bool) isFeeExempt;
+    mapping (address => bool) isTxLimitExempt;
+    mapping (address => bool) isTimelockExempt;
+    mapping (address => bool) isDividendExempt;
+}
