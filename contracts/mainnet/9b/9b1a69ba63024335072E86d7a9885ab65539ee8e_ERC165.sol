e064d7ff05836d85a31e27ba8c14703e8a5abbd9
minion <minion>
2022-12-16 17:06:04 +0000

    with 💕

---
+++ contracts/mainnet/9b/9b1a69ba63024335072E86d7a9885ab65539ee8e_ERC165.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import { ERC165Storage } from "./ERC165Storage.sol";
+
+contract ERC165 is IERC165 {
+    using ERC165Storage for ERC165Storage.Layout;
+
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(bytes4 interfaceId) public view returns (bool) {
+        return ERC165Storage.layout().isSupportedInterface(interfaceId);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+library ERC165Storage {
+    bytes32 internal constant STORAGE_SLOT = keccak256("diamond.standard.introspection.storage");
+
+    struct Layout {
+        mapping(bytes4 => bool) supportedInterfaces;
+    }
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+
+    function isSupportedInterface(Layout storage l, bytes4 interfaceId) internal view returns (bool) {
+        return l.supportedInterfaces[interfaceId];
+    }
+
+    function setSupportedInterface(Layout storage l, bytes4 interfaceId, bool status) internal {
+        require(interfaceId != 0xffffffff, "ERC165: invalid interface id");
+        l.supportedInterfaces[interfaceId] = status;
+    }
+}
