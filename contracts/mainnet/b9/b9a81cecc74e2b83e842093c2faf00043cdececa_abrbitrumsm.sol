a37c12f3c55158aeb52db35c4d5870b1aa0b0959
minion <minion>
2023-01-15 21:06:21 +0000

    with 💕

---
+++ contracts/mainnet/b9/b9a81cecc74e2b83e842093c2faf00043cdececa_abrbitrumsm.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at Arbiscan on 2023-01-15
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract abrbitrumsm {
+	function getHelloWorld() external pure returns (string memory) {
+			return "arb one love";
+
+	}
+}
