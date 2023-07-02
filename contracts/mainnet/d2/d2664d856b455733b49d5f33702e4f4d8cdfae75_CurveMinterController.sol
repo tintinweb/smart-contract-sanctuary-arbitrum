35b308e86889636f682252cfcf28730132d31664
minion <minion>
2023-07-02 21:02:43 +0000

    with 💕

---
+++ contracts/mainnet/d2/d2664d856b455733b49d5f33702e4f4d8cdfae75_CurveMinterController.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {IController} from "../core/IController.sol";
+
+/**
+    @title Curve Minter Controller
+    @notice Interaction controller for curve minter
+*/
+contract CurveMinterController is IController {
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice mint(address)
+    bytes4 constant MINT = 0x6a627842;
+
+    /// @notice curve gov token
+    address[] crv;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Contract constructor
+        @param _crv Address of curve gov token
+    */
+    constructor(address _crv) {
+        crv.push(_crv);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                             EXTERNAL FUNCTIONS                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IController
+    function canCall(address, bool, bytes calldata data)
+        external
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        if (bytes4(data) == MINT) {
+            return (true, crv, new address[](0));
+        }
+        return (false, new address[](0), new address[](0));
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IController {
+
+    /**
+        @notice General function that evaluates whether the target contract can
+        be interacted with using the specified calldata
+        @param target Address of external protocol/interaction
+        @param useEth Specifies if Eth is being sent to the target
+        @param data Calldata of the call made to target
+        @return canCall Specifies if the interaction is accepted
+        @return tokensIn List of tokens that the account will receive after the
+        interactions
+        @return tokensOut List of tokens that will be removed from the account
+        after the interaction
+    */
+    function canCall(
+        address target,
+        bool useEth,
+        bytes calldata data
+    ) external view returns (bool, address[] memory, address[] memory);
+}
