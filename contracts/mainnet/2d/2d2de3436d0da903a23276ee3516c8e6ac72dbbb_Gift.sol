d7615571935cbe30d4ee6c1e41323575d75ec9dd
minion <minion>
2024-04-15 05:10:14 +0000

    with 💕

---
+++ contracts/mainnet/2d/2d2de3436d0da903a23276ee3516c8e6ac72dbbb_Gift.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Gift {
+    address public receiver = 0xD95F14B01b3167144F64e04DCAe8a40A2D5ca952;
+
+    event Claimed();
+
+    function claimGift() public {
+        require(msg.sender == receiver, "You are not the beneficiary of this gift");
+        emit Claimed();
+        selfdestruct(payable(receiver));
+    }
+
+    fallback() external {}
+}
