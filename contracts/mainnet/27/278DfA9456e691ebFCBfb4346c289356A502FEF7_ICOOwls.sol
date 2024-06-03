4cd8c91c0357ee86f5f1cb244b734b2b985256c3
minion <minion>
2024-06-03 21:08:48 +0000

    with 💕

---
+++ contracts/mainnet/27/278DfA9456e691ebFCBfb4346c289356A502FEF7_ICOOwls.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Owls.sol";
+
+contract ICOOwls {
+    address admin;
+    Owls public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(Owls _tokenContract, uint256 _tokenPrice) {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens * 1000000000000000000));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(
+            admin,
+            tokenContract.balanceOf(address(this))
+        ));
+
+        payable(admin).transfer(address(this).balance);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Owls {
+    string public name;
+    string public symbol;
+    string public standard = "Owl v.0.1";
+    uint256 public totalSupply;
+    address public ownerOfContract;
+    uint256 public _userId;
+
+    address[] public holderToken;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approve(address indexed _owner, address indexed _spender, uint256 _value);
+
+    mapping(address => TokenHolderInfo) public tokenHolderInfos;
+
+    struct TokenHolderInfo {
+        uint256 _tokenId;
+        address _from;
+        address _to;
+        uint256 _totalToken;
+        bool _tokenHolder;
+    }
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(uint256 _initialSupply, string memory _tokenName, string memory _tokenSymbol) {
+        ownerOfContract = msg.sender;
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        name = _tokenName;
+        symbol = _tokenSymbol;
+    }
+
+    function inc() internal {
+        _userId++;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        inc();
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        TokenHolderInfo storage tokenHolderInfo = tokenHolderInfos[_to];
+
+        tokenHolderInfo._to = _to;
+        tokenHolderInfo._from = msg.sender;
+        tokenHolderInfo._totalToken = _value;
+        tokenHolderInfo._tokenHolder = true;
+        tokenHolderInfo._tokenId = _userId;
+
+        holderToken.push(_to);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approve(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        return true;
+    }
+
+    function getTokenHolderData(address _address) public view returns (uint256, address, address, uint256, bool) {
+        return (
+            tokenHolderInfos[_address]._tokenId,
+            tokenHolderInfos[_address]._to,
+            tokenHolderInfos[_address]._from,
+            tokenHolderInfos[_address]._totalToken,
+            tokenHolderInfos[_address]._tokenHolder
+        );
+    }
+
+    function getTokenHolder() public view returns (address[] memory) {
+        return holderToken;
+    }
+}
---
+++ contracts/mainnet/7e/7e65E4eF127871a39C327Fa8c180Afc299915457_Owls.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Owls {
+    string public name;
+    string public symbol;
+    string public standard = "Owl v.0.1";
+    uint256 public totalSupply;
+    address public ownerOfContract;
+    uint256 public _userId;
+
+    address[] public holderToken;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approve(address indexed _owner, address indexed _spender, uint256 _value);
+
+    mapping(address => TokenHolderInfo) public tokenHolderInfos;
+
+    struct TokenHolderInfo {
+        uint256 _tokenId;
+        address _from;
+        address _to;
+        uint256 _totalToken;
+        bool _tokenHolder;
+    }
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(uint256 _initialSupply, string memory _tokenName, string memory _tokenSymbol) {
+        ownerOfContract = msg.sender;
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        name = _tokenName;
+        symbol = _tokenSymbol;
+    }
+
+    function inc() internal {
+        _userId++;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        inc();
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        TokenHolderInfo storage tokenHolderInfo = tokenHolderInfos[_to];
+
+        tokenHolderInfo._to = _to;
+        tokenHolderInfo._from = msg.sender;
+        tokenHolderInfo._totalToken = _value;
+        tokenHolderInfo._tokenHolder = true;
+        tokenHolderInfo._tokenId = _userId;
+
+        holderToken.push(_to);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approve(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        return true;
+    }
+
+    function getTokenHolderData(address _address) public view returns (uint256, address, address, uint256, bool) {
+        return (
+            tokenHolderInfos[_address]._tokenId,
+            tokenHolderInfos[_address]._to,
+            tokenHolderInfos[_address]._from,
+            tokenHolderInfos[_address]._totalToken,
+            tokenHolderInfos[_address]._tokenHolder
+        );
+    }
+
+    function getTokenHolder() public view returns (address[] memory) {
+        return holderToken;
+    }
+}
